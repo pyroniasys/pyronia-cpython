c09ea8916fb882f6c8038ec4ae957610790e38c1
Marcela M <msmelara@gmail.com>
2017-10-18 10:59:54 -0400

    Add pass-by-reference test, improve test output

--- test/memtestlib.py
+++ test/memtestlib.py
@@ -1,14 +1,21 @@
 import memtestlib_native
 
-# test 1: accessing a native global variable
-def add_12(x):
-    print("Global variable from native: "+str(memtestlib_native.global_var))
-    memtestlib_native.global_var = 11
-    return x + memtestlib_native.global_var
-
-# test 2: trying to change the value of an immutable object in native
+# test 1: trying to change the value of an immutable object in native
 def add_4(x):
     y = 4
     memtestlib_native.pass_by_value(y)
     assert(y == 4)
     return x + y
+
+# test 2: trying to change the value of a mutable object in native
+def add_5(x):
+    y = [5]
+    memtestlib_native.pass_by_ref(y)
+    assert(y[0] != 5)
+    return x + y[0]
+
+# test 3: accessing a native global variable
+def add_12(x):
+    print("Global variable from native: "+str(memtestlib_native.global_var))
+    memtestlib_native.global_var = 11
+    return x + memtestlib_native.global_var
--- test/testapp.py
+++ test/testapp.py
@@ -23,14 +23,22 @@ attacklib.newprint1("hi there")
 # from a native library
 x = 3
 res = attacklib.add_3(x)
-print(str(x)+" + 3 = "+str(res))
+print("Adding 3 to "+str(x)+"; want "+str(x+3)+", get "+str(res))
 
 print("\n---- Memory interaction testing ----")
 
 import memtestlib
 
-print("Accessing a native global variable: ")
-print(str(x)+" + 12 = "+str(memtestlib.add_12(x)))
+# test 1: A native lib shouldn't be able to change the value of an
+# immutable Python object (corresponds to pass-by-value)
+print("Trying to change the value of an immutable object (expect AssertionError if the change succeeds)")
+print("Adding 4 to "+str(x)+"; want "+str(x+4)+", get "+str(memtestlib.add_4(x)))
 
-print("Trying to change the value of an immutable object (i.e. pass-by-value)")
-print(str(x)+" + 4 = "+str(memtestlib.add_4(x)))
+# test 2: A native lib should be able to change the value of a
+# mutable Python object (corresponds to pass-by-reference)
+print("Trying to change the value of a mutable object (expect AssertionError if the change fails)")
+print("Adding 5 to "+str(x)+"; want "+str(x+5)+", get "+str(memtestlib.add_5(x)))
+
+# test 3: Python should be able to change the value of a native global
+print("Accessing a native global variable: ")
+print("Adding 12 to "+str(x)+"; want "+str(x+12)+", get "+str(memtestlib.add_12(x)))
