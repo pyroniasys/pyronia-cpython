2fa772b7cf6dde4c8428aad3686d79c1a6e575ab
Marcela S. Melara <msmelara@gmail.com>
2018-02-02 23:06:04 -0800

    Move memory tests to mem-tests branch

--- test/setup.py
+++ test/setup.py
@@ -3,10 +3,7 @@ from distutils.core import setup, Extension
 module1 = Extension('attacklib_native',
                     sources = ['attacklib_native.c'])
 
-module2 = Extension('memtestlib_native',
-                    sources = ['memlib_native.c'])
-
 setup (name = 'FrameHackProto',
        version = '1.0',
        description = 'Frame hacking from native lib prototype',
-       ext_modules = [module1, module2])
+       ext_modules = [module1])
--- test/testapp.py
+++ test/testapp.py
@@ -24,34 +24,3 @@ attacklib.newprint1("hi there")
 x = 3
 res = attacklib.add_3(x)
 print("Adding 3 to "+str(x)+"; want "+str(x+3)+", get "+str(res))
-
-print("\n---- Memory interaction testing ----")
-
-import memtestlib
-
-# test 1: A native lib shouldn't be able to change the value of an
-# immutable Python object (corresponds to pass-by-value)
-print("test 1: Trying to change the value of an immutable object (expect AssertionError if the change succeeds)")
-print("Adding 4 to "+str(x)+"; want "+str(x+4)+", get "+str(memtestlib.add_4(x)))
-
-# test 2: A native lib should be able to change the value of a
-# mutable Python object (corresponds to pass-by-reference)
-print("test 2: Trying to change the value of a mutable object (expect AssertionError if the change fails)")
-print("Adding 5 to "+str(x)+"; want "+str(x+5)+", get "+str(memtestlib.add_5(x)))
-
-# test 3: Python should be able to change the value of a native global
-print("test 3: Accessing a native global variable: ")
-print("Adding 12 to "+str(x)+"; want "+str(x+12)+", get "+str(memtestlib.add_12(x)))
-
-# test 4: The native library will call back into the print_result function
-# in the main Python program
-def print_result(x):
-    print(x)
-
-print("test 4: Making a callback from native into the main Python program:")
-memtestlib.sum(3, print_result)
-
-# test 5: Perform a longjump from a native back into the print_result function
-# in the main Python program
-print("test 5: longjmp'ing from native into the main Python program:")
-memtestlib.prettify("hey you", print_result)
