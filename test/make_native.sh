19855b7c35246f59e6f72f0d1e87cf825d16c157
Marcela S. Melara <msmelara@gmail.com>
2018-02-02 23:27:54 -0800

    Remove frame hacks

--- test/make_native.sh
+++ test/make_native.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 python3 setup.py build
-mv build/lib.*/attacklib_native.cpython-35m-*.so .
 mv build/lib.*/memtestlib_native.cpython-35m-*.so .
--- test/setup.py
+++ test/setup.py
@@ -1,12 +1,9 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('attacklib_native',
-                    sources = ['attacklib_native.c'])
-
-module2 = Extension('memtestlib_native',
+module1 = Extension('memtestlib_native',
                     sources = ['memlib_native.c'])
 
-setup (name = 'FrameHackProto',
+setup (name = 'MemoryInteractionProto',
        version = '1.0',
-       description = 'Frame hacking from native lib prototype',
-       ext_modules = [module1, module2])
+       description = 'Memory interaction tests from native lib prototype',
+       ext_modules = [module1])
