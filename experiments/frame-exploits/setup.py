cf8c940a01f7ad136bb5d046a56bae950506b163
Marcela Melara <msmelara@gmail.com>
2018-05-04 15:59:34 -0400

    Demonstrate possible exploits Python stack frames (#12)

---
+++ experiments/frame-exploits/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('attacklib_native',
+                    sources = ['attacklib_native.c'])
+
+setup (name = 'FrameHackProto',
+       version = '1.0',
+       description = 'Frame hacking from native lib prototype',
+       ext_modules = [module1])
