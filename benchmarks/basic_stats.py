dad95c216b1e14210cea526c52ef897d9948f38b
Marcela S. Melara <msmelara@gmail.com>
2019-07-23 19:51:49 -0700

    Compute e2e and syscal overheads per app

--- benchmarks/basic_stats.py
+++ benchmarks/basic_stats.py
@@ -16,7 +16,7 @@ def get_e2e_latency_stats(latencies):
     for l in latencies:
         run_data = l.split(' ')
         exec_times.append(float(run_data[0].strip()))
-        max_dep_depth.append(int(run_data[1].strip()))
+        max_dep_depth.append(int(run_data[1].strip()[:-1]))
 
     stats = OrderedDict()
     stats['min'] = (("%.4f" % min(exec_times)), ("%d" % min(max_dep_depth)))
@@ -26,7 +26,55 @@ def get_e2e_latency_stats(latencies):
     stats['stddev'] = (("%.4f" % stdev(exec_times)), ("%.4f" % stdev(max_dep_depth)))
     return stats
 
+syscalls = ['open', 'fopen', 'connect']
+
 def get_syscall_latency_stats(latencies):
+    open_times = []
+    fopen_times = []
+    connect_times = []
+    open_idx = 0
+    fopen_idx = 1
+    connect_idx = 2
+    for l in latencies:
+        run_data = l.split(',')
+        open_bench = run_data[open_idx].split(' ')
+        fopen_bench = run_data[fopen_idx].split(' ')
+        connect_bench = run_data[connect_idx].split(' ')
+        open_num = float(open_bench[0].strip())
+        fopen_num = float(fopen_bench[0].strip())
+        connect_num = float(connect_bench[0].strip())
+        if open_num > 0.0:
+            open_times.append(float(open_bench[1].strip())/open_num)
+        if fopen_num > 0.0:
+            fopen_times.append(float(fopen_bench[1].strip())/fopen_num)
+        if connect_num > 0.0:
+            connect_times.append(float(connect_bench[1].strip())/connect_num)
+
+    if len(open_times) == 0:
+        open_times = [0.0, 0.0]
+    if len(fopen_times) == 0:
+        fopen_times = [0.0, 0.0]
+    if len(connect_times) == 0:
+        connect_times = [0.0, 0.0]
+
+    stats = OrderedDict()
+    stats['open'] = OrderedDict()
+    stats['fopen'] = OrderedDict()
+    stats['connect'] = OrderedDict()
+    stats['open']['times'] = open_times;
+    stats['fopen']['times'] = fopen_times;
+    stats['connect']['times'] = connect_times
+
+    for s in stats:
+        stats[s]['stats'] = OrderedDict()
+        stats[s]['stats']['min'] = "%.2f" % min(stats[s]['times'])
+        stats[s]['stats']['mean'] = "%.2f" % mean(stats[s]['times'])
+        stats[s]['stats']['median'] = "%.2f" % median(stats[s]['times'])
+        stats[s]['stats']['max'] = "%.2f" % max(stats[s]['times'])
+        stats[s]['stats']['stddev'] = "%.2f" % stdev(stats[s]['times'])
+    return stats
+
+def get_iters_latency_stats(latencies):
     iter_times = OrderedDict()
     for i in range(0, 100):
         iter_times[str(i)] = []
@@ -55,21 +103,6 @@ def get_syscall_latency_stats(latencies):
     stats['stddev'] = iter_stddev
     return stats
 
-def get_iters_latency_stats(latencies):
-    exec_times = []
-    max_dep_depth = []
-    for l in latencies:
-        run_data = l.split(' ')
-        exec_times.append(float(run_data[0].strip()))
-        max_dep_depth.append(int(run_data[1].strip()))
-
-    stats = OrderedDict()
-    stats['min'] = (("%.4f" % min(exec_times)), ("%d" % min(max_dep_depth)))
-    stats['mean'] = (("%.4f" % mean(exec_times)), ("%.2f" % mean(max_dep_depth)))
-    stats['median'] = (("%.4f" % median(exec_times)), ("%.2f" % median(max_dep_depth)))
-    stats['max'] = (("%.4f" % max(exec_times)), ("%d" % max(max_dep_depth)))
-    stats['stddev'] = (("%.4f" % stdev(exec_times)), ("%.4f" % stdev(max_dep_depth)))
-
 for d in datasets:
     for bt in bench_types:
         app_stats = OrderedDict()
@@ -87,6 +120,6 @@ for d in datasets:
 
             app_stats[a] = stats
 
-        out = open(app_path+'/app_'+bt+'-'+d+'_stats.txt', 'w+')
+        out = open(app_path+'/benchmarks/app_'+bt+'-'+d+'_stats.txt', 'w+')
         json.dump(app_stats, out, indent=4)
         out.close()
--- benchmarks/latency_overheads.py
+++ benchmarks/latency_overheads.py
@@ -6,12 +6,12 @@ def calc_diff(new, orig):
     return float(new)-float(orig)
 
 def calc_percent(new, orig):
-    return (calc_diff(new, orig))/float(orig)
+    return ((calc_diff(new, orig))/float(orig))*100.0
 
 def calc_multi(new, orig):
     return int(float(new)/float(orig))
 
-app_path = '/Users/marcela/Research/lib-isolation/cpython'
+app_path = '/home/marcela/Research/lib-isolation/cpython'
 
 apps = ['hello', 'twitterPhoto', 'alexa', 'plant_watering']
 
@@ -19,30 +19,37 @@ bench_types = ['e2e-latency', 'syscall-latency']
 
 datasets = ['nopyr', 'pyr']
 
+syscalls = ['open', 'fopen', 'connect']
+
 overheads = OrderedDict()
 overheads['mean e2e latency overhead'] = OrderedDict()
 overheads['mean syscall latency overhead'] = OrderedDict()
 for bt in bench_types:
     nopyr_data = dict()
     pyr_data = dict()
-    with open(app_path+'/app_'+bt+'-pyr_stats.txt', 'r') as fp:
+    with open(app_path+'/benchmarks/app_'+bt+'-pyr_stats.txt', 'r') as fp:
         pyr_data = json.load(fp, object_pairs_hook=OrderedDict)
-    with open(app_path+'/app_'+bt+'-nopyr_stats.txt', 'r') as fp:
+    with open(app_path+'/benchmarks/app_'+bt+'-nopyr_stats.txt', 'r') as fp:
         nopyr_data = json.load(fp, object_pairs_hook=OrderedDict)
     
-    for a in apps:
-        # gather pyr and no_pyr lists
-        if bt == 'e2e-latency':
+    # gather pyr and no_pyr lists
+    if bt == 'e2e-latency':
+        for a in apps:
             pyr_mean = float(pyr_data[a]['mean'][0])/1000000.0
             nopyr_mean = float(nopyr_data[a]['mean'][0])/1000000.0
 
-            overheads['mean e2e latency overhead'][a] = ("%.2f seconds (%.1f %)" % (calc_diff(pyr_mean, nopyr_mean), calc_percent(pyr_mean, nopyr_mean)))
-        elif bt == 'syscall-latency':
-            
-            overheads['mean module-only overhead'][a] = ("%.2f seconds (%dx)" % (calc_diff(pyr_mean, nopyr_mean), calc_multi(pyr_mean, nopyr_mean)))
+            overheads['mean e2e latency overhead'][a] = ("%.2f seconds (%.1f%%)" % (calc_diff(pyr_mean, nopyr_mean), calc_percent(pyr_mean, nopyr_mean)))
+    elif bt == 'syscall-latency':
+        overheads['mean syscall latency overhead']['open'] = OrderedDict()
+        overheads['mean syscall latency overhead']['fopen'] = OrderedDict()
+        overheads['mean syscall latency overhead']['connect'] = OrderedDict()
+        for a in apps:
+            for s in syscalls:
+                pyr_mean = float(pyr_data[a][s]['stats']['mean'])
+                nopyr_mean = float(nopyr_data[a][s]['stats']['mean'])
+                if nopyr_mean > 0.0:
+                    overheads['mean syscall latency overhead'][s][a] = ("%.1f%% (%.2f us)" % (calc_percent(pyr_mean, nopyr_mean), calc_diff(pyr_mean, nopyr_mean)))
 
-    pyr_mem_dict = read_mprofile_file(a+'/mprofile_'+a+'-medians-pyr.dat')
-    nopyr_mem_dict = read_mprofile_file(a+'/mprofile_'+a+'-medians-nopyr.dat')
-    overheads['max median mem overhead'][a] = ("%.2f MB (%dx)" % (calc_diff(max(pyr_mem_dict['mem_usage']), max(nopyr_mem_dict['mem_usage'])), calc_multi(max(pyr_mem_dict['mem_usage']), max(nopyr_mem_dict['mem_usage']))))
-
-json.dump(overheads, sys.stdout, indent=4)
+out = open(app_path+'/benchmarks/pyronia_overheads.txt', 'w+')
+json.dump(overheads, out, indent=4)
+out.close()
