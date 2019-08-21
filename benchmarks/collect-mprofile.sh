83ba408547755b1582c84740ed15beae4aaafb1e
Marcela Melara <msmelara@gmail.com>
2019-08-21 19:52:46 -0400

    Collect iter totoal mem usage for two apps

--- benchmarks/collect-mprofile.sh
+++ benchmarks/collect-mprofile.sh
@@ -3,26 +3,23 @@
 runs=2
 
 if [ $# -ne 1 ]; then
-    echo 'Usage: ./collect_pyr_benchmarks.sh <benchmark type>'
+    echo 'Usage: ./collect-mprofile.sh <benchmark type>'
     exit 0
 fi
     
 bench_type=$1
 
-apps="twitterPhoto"
+apps="alexa"
 apps_path='/home/pyronia/libpyronia/apps'
 lib_prof_temp='/home/pyronia/cpython/profiles/home.pyronia.cpython.pyronia_build.python-lib.prof'
 
 for a in $apps; do
     cp $lib_prof_temp'.'$a $lib_prof_temp
-    ../load_python_profile
+    /home/pyronia/cpython/load_python_profile
     x=1
     while [ $x -le $runs ]; do
-	export LD_PRELOAD=/lib/x86_64-linux-gnu/libpyronia_opt.so
-        ../pyronia_build/python $apps_path'/'$a'/'$a'.py'
+        mprof run --include-children -o $a'/'$a'-mprofile-'$x'-'$bench_type'.data' /home/pyronia/cpython/run_pyr_python.sh $apps_path'/'$a'/'$a'.py'
         echo "Finished run $x for app $a"
 	x=$(($x+1))
-	../load_python_profile
     done
-    mv $apps_path'/'$a'/'$a'.py.data' $a'/'$a'-'$bench_type'.data'
 done
--- run_pyr_python.sh
+++ run_pyr_python.sh
@@ -2,4 +2,4 @@
 
 APP=$1
 
-LD_PRELOAD=/lib/x86_64-linux-gnu/libpyronia_opt.so /home/pyronia/cpython/pyronia_build/python /home/pyronia/libpyronia/apps/$APP/$APP.py
+LD_PRELOAD=/lib/x86_64-linux-gnu/libpyronia_opt.so /home/pyronia/cpython/pyronia_build/python $APP
